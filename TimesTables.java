756f4244ad2a8676433806c621aa62fd1140cbe6
김단이 <kimdn0907@gamil.com>
2026-05-05 11:03:24 +0900

    프로젝트 공유 초기화

---
+++ TimesTables.java
@@ -0,0 +1,34 @@
+
+/**
+ * 객체의 이름과 n단을 출력해달라고 요청하면 호출된 이름을 가진 객체가 n단을 출력해주는 프로그램
+ * (단, n은 1~9 사이)
+ *
+ * @author (2025320031 김단이)
+ * @version (2026.05.05)
+ */
+public class TimesTables
+{
+    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
+    private int x;
+
+    /**
+     * TimesTables 클래스의 객체 생성자
+     */
+    public TimesTables()
+    {
+        // 인스턴스 변수 초기화
+        x = 0;
+    }
+
+    /**
+     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public int sampleMethod(int y)
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+}
