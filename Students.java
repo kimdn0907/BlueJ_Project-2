cb7dc60606ff146b9e9af543f3f895b5e2c5abed
정한비 <hanbijeong2@gmail.com>
2026-05-05 14:36:27 +0900

    student 클래스 생성

--- Students.java
+++ Students.java
@@ -1,23 +1,24 @@
 
 /**
- * 객체의 이름과 n단을 출력해달라고 요청하면 호출된 이름을 가진 객체가 n단을 출력해주는 프로그램
+ * 객체의 이름과 n단을 출력해달라고 요청하면 호출된 이름을 가진 객체가 n단을 출력해주는 
+ * 프로그램
+ * 
  * (단, n은 1~9 사이)
  *
- * @author (2025320031 김단이)
+ * @author (2025320031 김단이, 2025320016 정한비, 2025320020 송영재)
  * @version (2026.05.05)
  */
 public class Students
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
+    private String name;
+    private String studentId;
 
     /**
      * TimesTables 클래스의 객체 생성자
      */
-    public Students()
-    {
-        // 인스턴스 변수 초기화
-        x = 0;
+    public Students(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
     }
 
     /**
@@ -26,9 +27,21 @@ public class Students
      * @param  y  메소드의 샘플 파라미터
      * @return    x 더하기 y의 결과값을 반환
      */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하세요.
-        return x + y;
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
+    public String getTimesTables(int TimesTables) {
+        String result = name + ":";
+        for (int i = 1; i<=9; i++) {
+            result = result + TimesTables + "x" + i + "=" + (TimesTables * i) + " ";
+        }
+        return result;
     }
 }
